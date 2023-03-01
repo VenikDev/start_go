73889f14a3a3156acf67e0333700d61d8d4d44ad
Alexander <a.oshurkov@iridi.tech>
2023-03-01 15:11:16 +0500

    - add bingo

--- main.go
+++ main.go
@@ -1,22 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
 func main() {
-	ch := make(chan int64, 10)
 
-	for idx := 0; idx < 10; idx++ {
-		go func(idx int) {
-			ch <- int64(idx)
-		}(idx)
-		time.Sleep(500 * time.Millisecond)
-	}
-
-	for idx := 0; idx < 10; idx++ {
-		data := <-ch
-		fmt.Println(data)
-	}
 }
